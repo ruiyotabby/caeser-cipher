8398953a2af4d778681e1867430d4e90cf0a6d6b
Ruiyotabby <ruiyotabby@gmail.com>
2022-09-21 22:00:23 +0300

    Add regex to the cipher function

--- cipher.rb
+++ cipher.rb
@@ -1,10 +1,14 @@
 def ceaser_cipher(sentence, shift)
   ciphered_sentence = sentence.split("").map do |char|
-    char = char.ord + shift
-    char.chr
+    unless /[[:alpha:]]/.match(char)
+      char = char
+    else
+      char = char.ord + shift
+      char.chr
+    end
   end
   p ciphered_sentence.join('')
 
 end
 
-ceaser_cipher('hello ds', 4)
+ceaser_cipher('Hello d_s!', 4)
